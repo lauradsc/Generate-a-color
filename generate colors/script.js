c4a3e7a297a2cec251322c4c6c4a80a16e4a9ba8
Laura Costa <99484087+lauradsc@users.noreply.github.com>
2022-10-10 10:58:16 -0300

    Add files via upload

---
+++ generate colors/script.js
@@ -0,0 +1,9 @@
+const setcor = () => { 
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor; 
+  
+  }
+  
+  genc.addEventListener("click", setcor);
+  setcor();
